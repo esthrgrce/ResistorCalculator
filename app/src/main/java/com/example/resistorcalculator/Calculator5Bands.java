789d954486f0e32c7263b19b6ad614a9f4f316fc
esthrgrce <esthergrace.falate@msugensan.edu.ph>
2021-05-07 21:26:44 +0800

    initial commit

---
+++ app/src/main/java/com/example/resistorcalculator/Calculator5Bands.java
@@ -0,0 +1,14 @@
+package com.example.resistorcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calculator5Bands extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator5_bands);
+    }
+}
---
+++ app/src/main/java/com/example/resistorcalculator/Calculator6Bands.java
@@ -0,0 +1,14 @@
+package com.example.resistorcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calculator6Bands extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator6_bands);
+    }
+}
